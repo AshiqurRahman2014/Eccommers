7bfef11a924c0665540bde93ac6a4b0a4d35c6b0
Ashiqur Rahman <mdashiqur110505@gmail.com>
2025-09-06 18:55:25 +0600

    Add project files.

---
+++ Eccommers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eccommers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
